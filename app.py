b1fae3643dab5fd703acb0a35df9e87c7788e1a9
unknown <me@stylite.me>
2022-06-23 01:24:53 -0400

    Fix icy_streamtitle

--- app.py
+++ app.py
@@ -9,9 +9,10 @@ ip = IcyParser()
 url = 'http://ice1.somafm.com/groovesalad-128-mp3'
 ip.getIcyInformation(url)
 
+
 @application.route("/")
 def index():
-    songname = ip.icy_streamtitle.strip("';StreamUrl='")
+    songname = ip.icy_streamtitle.replace("';StreamUrl='http://somafm.com/logos/512/groovesalad512.png", "")
     return render_template("index.html", songname=songname)
 
 if __name__ == "__main__":
